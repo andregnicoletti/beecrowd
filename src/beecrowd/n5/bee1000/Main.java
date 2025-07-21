5ca0172d50bd1e2613ef7c0939d4c6c0e3ae9cf1
André Nicoletti <andregnicoletti@gmail.com>
2025-07-21 11:13:49 -0300

    bee1000

---
+++ src/beecrowd/n5/bee1000/Main.java
@@ -0,0 +1,15 @@
+package beecrowd.n5.bee1000;
+
+public class Main {
+
+    /**
+     * https://judge.beecrowd.com/pt/problems/view/1000
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
