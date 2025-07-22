7c3fb63727815bb0c4115bd48400d376782444e3
André Nicoletti <andregnicoletti@gmail.com>
2025-07-22 09:49:26 -0300

    bee1009

---
+++ src/beecrowd/n2/bee1009/Main.java
@@ -0,0 +1,34 @@
+package beecrowd.n2.bee1009;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * https://judge.beecrowd.com/pt/problems/view/1009
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String employeeName = sc.next();
+		double employeeSalary = sc.nextDouble();
+		double employeeSales = sc.nextDouble();
+		double bonus = 0.0;
+
+		if (employeeSales > 0) {
+			bonus = employeeSales * 0.15;
+		}
+
+		BigDecimal salaryTotal = new BigDecimal(employeeSalary + bonus).setScale(2, RoundingMode.HALF_UP);
+
+		System.out.println("TOTAL = R$ " + salaryTotal);
+
+		sc.close();
+
+	}
+
+}
