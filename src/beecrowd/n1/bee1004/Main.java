3520dbb10b977f366fbcd922e89fd74d48454286
André Nicoletti <andregnicoletti@gmail.com>
2025-07-21 23:10:45 -0300

    bee1004

---
+++ src/beecrowd/n1/bee1004/Main.java
@@ -0,0 +1,19 @@
+package beecrowd.n1.bee1004;
+
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * https://judge.beecrowd.com/pt/problems/view/1004
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("PROD = " + (sc.nextInt() * sc.nextInt()));
+		sc.close();
+
+	}
+
+}
