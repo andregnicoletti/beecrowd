901af49c3d943e8b37f70dad56f2e1bfa19edefa
André Nicoletti <andregnicoletti@gmail.com>
2025-07-21 23:00:22 -0300

    bee1003

---
+++ src/beecrowd/n1/bee1003/Main.java
@@ -0,0 +1,17 @@
+package beecrowd.n1.bee1003;
+
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * https://judge.beecrowd.com/pt/problems/view/1003
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("SOMA = " + (sc.nextInt() + sc.nextInt()));
+		sc.close();
+	}
+
+}
