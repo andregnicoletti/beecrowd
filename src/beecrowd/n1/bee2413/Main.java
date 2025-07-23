692abd47790ec61167ab067ba0b3859cf5f5e1b9
André Nicoletti <andregnicoletti@gmail.com>
2025-07-23 08:32:11 -0300

    bee2413

---
+++ src/beecrowd/n1/bee2413/Main.java
@@ -0,0 +1,20 @@
+package beecrowd.n1.bee2413;
+
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * https://judge.beecrowd.com/pt/problems/view/2413
+	 */
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int clicks = sc.nextInt();
+		sc.close();
+
+		System.out.println((clicks * 2) * 2);
+
+	}
+}
